1e1585884fbb5e77a5905aa979a5a254dcba2d1d
sonechqa <sona.simonova@list.ru>
2022-05-02 13:11:52 +0300

    init

---
+++ web/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Docker <?php echo "fsafasjfsabfajsbgfasnhgfsakj sfa sfasfa s af???" ?></title>
+    </head>
+    <body>
+        sfafas fas 
+        <h1>Docker <?php echo "fsafasjfsabfajsbgfasnhgfsakj sfa sfasfa s af???" ?></h1>
+    </body>
+</html>
