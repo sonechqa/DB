10fbd30197265575c788d3e4c6a6180c564031ad
sonechqa <sona.simonova@list.ru>
2022-05-05 14:36:42 +0300

    Операторы, условия, циклы

--- web/public/index.php
+++ web/public/index.php
@@ -120,6 +120,97 @@
     }
     echo longdate(time())
     ?>
+
+    <?php
+    echo "<br>";
+    $a = 5;
+    $b = 5;
+
+    if ($a > $b) echo "$a больше $b<br>";
+    if ($a < $b) echo "$a меньше $b<br>";
+    if ($a >= $b) echo "$a больше или равно $b<br>";
+    if ($a <= $b) echo "$a меньше или равно $b<br>";
+    ?>
+
+    <?php
+    echo "<br>";
+    $a = 1;
+    $b = 0;
+
+    echo ($a and $b) . "<br>";
+    echo ($a or $b) . "<br>";
+    echo ($a xor $b) . "<br>";
+    echo !$a . "<br>";
+    ?>
+
+    <?php
+    /* echo "<br>";
+    $fuel = 10;
+
+    while ($fuel > 1) {
+        echo "Топлива ещё достаточно";
+    } */
+    ?>
+
+    <?php
+    /* echo "<br>";
+    $count = 0;
+
+    while (++$count <= 12) {
+        echo "Число $count, умноженное на 12, равно " . $count * 12 . "<br>";
+    }
+    ?>
+
+    <?php
+    echo "<br>";
+    $count = 1;
+    do {
+        echo "Число $count, умноженное на 12, равно " . $count * 12 . "<br>";
+    } while (++$count <= 12); */
+    ?>
+
+    <?php
+    echo "<br>";
+    for ($count = 1; $count <= 12; ++$count) {
+        echo "Число $count, умноженное на 12, равно " . $count * 12 . "<br>";
+    }
+    ?>
+
+    <?php
+    echo "<br>";
+    $j = 10;
+
+    while ($j > -10) {
+        $j--;
+
+        if ($j == 0) continue;
+
+        echo (10 / $j) . "<br>";
+    }
+    ?>
+
+    <?php
+    echo "<br>";
+    $a = 56;
+    $b = 12;
+    $c = (int) ($a / $b);
+
+    echo $c;
+    ?>
+
+    <?php
+    echo "<br>";
+    echo fix_names("WILLIAM", "henry", "gatES");
+
+    function fix_names($n1, $n2, $n3)
+    {
+        $n1 = ucfirst(strtolower($n1));
+        $n2 = ucfirst(strtolower($n2));
+        $n3 = ucfirst(strtolower($n3));
+
+        return $n1 . " " . $n2 . " " . $n3;
+    }
+    ?>
 </body>
 
 </html>
