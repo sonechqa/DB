b88575ff8494e5afd024f9645b773d9cfc6a05ae
sonechqa <sona.simonova@list.ru>
2022-05-19 09:16:20 +0300

    Frontend with cors

---
+++ web/public/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  async mounted() {
+    const { data } = await axios.post('http://localhost:8000/endpoints/getFirstBook.php');
+    this.msg = data;
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
